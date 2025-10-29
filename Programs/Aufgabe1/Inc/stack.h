517bf43b264c31024bd9a87f335d6b799ab0300c
Hakimi404 <104299869+Hakimi404@users.noreply.github.com>
2025-10-29 19:29:57 +0100

    Update stack.h

--- Programs/Aufgabe1/Inc/stack.h
+++ Programs/Aufgabe1/Inc/stack.h
@@ -1,10 +1,4 @@
-/**
- * @file stack.h
- * @author 
- * @brief Stack-Modul für den RPN-Taschenrechner.
- *        Dieses Modul implementiert die Stack-Verwaltung
- *        inklusive Push, Pop, Clear, Print, Duplicate und Swap.
- */
+
 
 #ifndef STACK_H
 #define STACK_H
@@ -29,34 +23,19 @@
  */
 int push(int wert);
 
-/**
- * @brief Entfernt den obersten Wert vom Stack und speichert ihn in *tmp.
- * @param tmp Pointer zur Speicherung des Werts.
- * @return STACK_OK oder Fehlercode.
- */
+
 int pop(int *tmp);
 
-/**
- * @brief Gibt den obersten Wert des Stacks aus, ohne ihn zu entfernen.
- * @return STACK_OK oder Fehlercode.
- */
+
 int printTop(void);
 
-/**
- * @brief Gibt den gesamten Stack-Inhalt aus, ohne ihn zu verändern.
- * @return STACK_OK oder Fehlercode.
- */
+
 int printAll(void);
 
-/**
- * @brief Löscht alle Einträge im Stack.
- */
+
 void clearStack(void);
 
-/**
- * @brief Dupliziert den obersten Eintrag des Stacks.
- * @return STACK_OK oder Fehlercode.
- */
+
 int duplicateTop(void);
 
 /**
