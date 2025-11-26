a4be5db580726d3aaea5537d6518a3c5df0e27f5
hakimi404 <104299869+Hakimi404@users.noreply.github.com>
2025-11-26 21:52:44 +0100

    kommentare fertig

--- Programs/Aufgabe2/Src/Gpio.c
+++ Programs/Aufgabe2/Src/Gpio.c
@@ -1,5 +1,5 @@
 #include "gpio.h"
 
-int readGPIOPin(GPIO_TypeDef *GPIOx, int pin) {
-    return (GPIOx->IDR & (1 << pin)) ? 1 : 0;
+int readGPIOPin(GPIO_TypeDef *GPIOx, int pin) { //Liest den Zustand (0 oder 1) eines Pins
+    return (GPIOx->IDR & (1 << pin)) ? 1 : 0; //Pin maskieren und 0 oder 1 zurückgeben
 }
--- Programs/Aufgabe2/Src/input.c
+++ Programs/Aufgabe2/Src/input.c
@@ -8,9 +8,10 @@
 unsigned int aft = 0, bef = 0, time = 0;
 
 int input_einlesen() {
-    aft = TIM2->CNT;
-    time += aft - bef;
-    bef = aft;
+    aft = TIM2->CNT; // Den aktuellen Timer Wert holen
+    time += aft - bef; // Die verstrichene Zeit aufaddieren
+    bef = aft; // Der Referenzwert für den nächsten Aufruf setzen
+
     /*int s0 = readGPIOPin(BUTTON_PORT, S0);
     int s1 = readGPIOPin(BUTTON_PORT, S1);
 
@@ -18,14 +19,14 @@ int input_einlesen() {
     if (s0 && !s1)  return PHASE_B;
     if (s0 && s1)   return PHASE_C;
     if (!s0 && s1)  return PHASE_D;*/
-    return GPIOF->IDR & 0x03;
+    return GPIOF->IDR & 0x03; //Maskiert die unteren 2 Bits (IN0, IN1) von Port F
 }
-
+//Prüft ob das Zeitlimit überschritten wurde
 int inputzeit() {
     if (time >= MAX) { time = 0; return 1; }
     return 0;
 }
-
+//Prüft Taster S7
 bool resetpressed() {
     int s7 = readGPIOPin(BUTTON_PORT, S7);
     return (s7 == 0);
